da63b671f584ff9e770ab958d25adc559ab7c246
mitizdev <mitizdev@gmail.com>
2024-02-06 21:53:25 +0530

    changes done

--- src/app/(root)/Teams/Teams.jsx
+++ src/app/(root)/Teams/Teams.jsx
@@ -11,70 +11,11 @@ const Teams = () => {
               <div class="team-image-wrapper">
                 <img
                   class="team-member-image"
-                  src="/images/teams/ritik.jpeg"
-				  height="130rem"
-                />
-              </div>
-              <p class="text-blk name">Ritik Raj</p>
-              <p class="text-blk position">CEO</p>
-              <p class="text-blk feature-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </p>
-              <div class="social-icons">
-                <a href="https://www.twitter.com" target="_blank">
-                  <img
-                    class="twitter-icon"
-                    src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/Icon.svg"
-                  />
-                </a>
-                <a href="https://www.facebook.com" target="_blank">
-                  <img
-                    class="facebook-icon"
-                    src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/Icon-1.svg"
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
-            <div class="card">
-              <div class="team-image-wrapper">
-                <img
-                  class="team-member-image"
-                  src="/images/teams/amit.jpeg"
-                />
-              </div>
-              <p class="text-blk name">Amit Singh</p>
-              <p class="text-blk position">CEO</p>
-              <p class="text-blk feature-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </p>
-              <div class="social-icons">
-                <a href="https://www.twitter.com" target="_blank">
-                  <img
-                    class="twitter-icon"
-                    src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/Icon.svg"
-                  />
-                </a>
-                <a href="https://www.facebook.com" target="_blank">
-                  <img
-                    class="facebook-icon"
-                    src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/Icon-1.svg"
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
-            <div class="card">
-              <div class="team-image-wrapper">
-                <img
-                  class="team-member-image"
                   src="/images/teams/anurag.jpeg"
                 />
               </div>
               <p class="text-blk name">Anurag Sinha</p>
-              <p class="text-blk position">CEO</p>
+              <p class="text-blk position">Founder</p>
               <p class="text-blk feature-text">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               </p>
@@ -99,11 +40,38 @@ const Teams = () => {
               <div class="team-image-wrapper">
                 <img
                   class="team-member-image"
-                  src="/images/teams/katherin.jpeg"
+                  src="/images/teams/ritik.jpeg"
+                  height="130rem"
                 />
               </div>
-              <p class="text-blk name">Katherine Langaford</p>
-              <p class="text-blk position">CEO</p>
+              <p class="text-blk name">Ritik Raj</p>
+              <p class="text-blk position">Co-Founder</p>
+              <p class="text-blk feature-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+              <div class="social-icons">
+                <a href="https://www.twitter.com" target="_blank">
+                  <img
+                    class="twitter-icon"
+                    src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/Icon.svg"
+                  />
+                </a>
+                <a href="https://www.facebook.com" target="_blank">
+                  <img
+                    class="facebook-icon"
+                    src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/Icon-1.svg"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+            <div class="card">
+              <div class="team-image-wrapper">
+                <img class="team-member-image" src="/images/teams/amit.jpeg" />
+              </div>
+              <p class="text-blk name">Amit Singh</p>
+              <p class="text-blk position">Co-Founder</p>
               <p class="text-blk feature-text">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               </p>
